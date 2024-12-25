4a34badb4605693fc7ff2501814adde232c9e4cf
mapi <rfriends2017@ymail.ne.jp>
2024-12-25 17:26:51 +0900

    Add files via upload

--- rfriends3/rfriends3_boot.sh
+++ rfriends3/rfriends3_boot.sh
@@ -1,19 +1,28 @@
 #!/bin/sh
-#
+# ------------------------------------------------
 # rfriends3 boot処理 for raspberry pi
 # 2023/08/11
-#
-usr=rpi
-#
-if [ -f /home/${usr}/rfriends3/rfriends3_boot.txt ]; then
-  mode=`head -1 /home/${usr}/rfriends3/rfriends3_boot.txt`
+# 2024/12/25
+# ------------------------------------------------
+dir=$(cd $(dirname $0);pwd)
+log=$dir/rfriends3_boot.log
+echo `date` rfriends_boot.sh start $dir > $log
+
+homedir=`cd $dir/../;pwd`
+user=`echo "$dir" | cut -d'/' -f3`
+echo homedir: $homedir user: $user >> $log
+
+sdir=s%rfriendshomedir%${homedir}%g
+susr=s%rfriendsuser%$user%g
+# ------------------------------------------------
+cd $dir
+if [ -f $dir/rfriends3_boot.txt ]; then
+  mode=`head -1 $dir/rfriends3_boot.txt`
   mode=`echo $mode`
 else
   mode=bis
 fi
-
-ip=`hostname -I | cut -d\  -f 1`
-port=8000
+echo mode: $mode >> $log
 #
 systemctl stop apache2
 systemctl stop php7.4-fpm
@@ -23,8 +32,9 @@ systemctl stop lighttpd
 # built in server
 #
 if [ ${mode} = "bis" ]; then
-    shl=/home/${usr}/rfriends3/rfriends3_server.sh
+    shl=$dir/rfriends3_server.sh
     su -l ${usr} -c "sh ${shl} ${ip}:${port} > /dev/null 2>&1" &
+    echo start built in server >> $log
 #
 # apache2
 #
@@ -34,6 +44,7 @@ elif [ ${mode} = "apache" ]; then
     echo '# rfrieds3' > ${conf}
     echo "Listen ${ip}:${port}" >> ${conf}
     systemctl start apache2
+    echo start apache2 >> $log
 #
 # nginx
 #
@@ -41,17 +52,16 @@ elif [ ${mode} = "nginx" ]; then
     systemctl start php7.4-fpm
     #systemctl start php8.1-fpm
     systemctl start nginx
+    echo start nginx >> $log
 #
 # lighttpd
 #
 elif [ ${mode} = "lighttpd" ]; then
-    cp /home/${usr}/rfriends3/lighttpd.conf /etc/lighttpd/.
-    cp /home/${usr}/rfriends3/15-fastcgi-php.conf /etc/lighttpd/conf-available/.
+    cat $dir/lighttpd.conf        | sed -e $sdir | sed -e $susr | sudo tee /etc/lighttpd/lighttpd.conf > /dev/null
+    cat $dir/15-fastcgi-php.conf  | sed -e $sdir | sudo tee /etc/lighttpd/conf-available/15-fastcgi-php.conf > /dev/null
     #chown ${usr}:${usr} /run/lighttpd
     systemctl start lighttpd
+    echo start lighttpd >> $log
 fi
 
-# add 2024/09/09 for wifi power management off
-/sbin/iwconfig wlan0 power off
-#/sbin/iw dev wlan0 set power_save off
-
+exit 0
