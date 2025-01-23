c92622674c9a0ca7c57218c97bb7110a2a41f897
mapi <rfriends2017@ymail.ne.jp>
2025-01-23 16:24:05 +0900

    Update sample.sh

--- sample.sh
+++ sample.sh
@@ -1,18 +1,30 @@
 #!/bin/bash
 # =========================================
-# install rfriends for ubuntu
+# install rfriends for linux
+#
+# sample.sh
+#
+# 1.0 2025/01/23 new
 # =========================================
-# 1.0 2025/01/03 new
 ver=1.0
 echo start $ver
 echo
 # -----------------------------------------
+distro=ubuntu
+cmd=apt-get
 optlighttpd="on"
 optsamba="on"
+# -----------------------------------------
+export distro
+export cmd
 export optlighttpd
 export optsamba
 #
-sh ubuntu_install.sh 2>&1 | tee ubuntu_install.log
+rm -rf rfriends3
+git clone http://github.com/rfriends/rfriends3.git
+cd rfriends3
+sh install_rfriends3.sh 2>&1 | tee install_rfriends3.log
+mv rfriends3 ../../
 # -----------------------------------------
 # finish
 echo
